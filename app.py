05380396ce23aaf99491660a1c5b72a20cc9b4be
LPiotr1 <piotr.lebelt1@gmail.com>
2023-07-03 10:08:04 +0200

    minor changes in layout

--- app.py
+++ app.py
@@ -3,12 +3,15 @@ from gevent.pywsgi import WSGIServer
 from flask_mysqldb import MySQL
 import redis
 import MySQLdb.cursors
-import datetime
-import time
+from datetime import datetime
+import time 
 import json
 import re
+import locale
 
 
+
+locale.setlocale(locale.LC_TIME, 'pl_PL')
 
 app = Flask(__name__)
 r = redis.Redis(host='192.168.100.18', port=6379, db=0, charset='utf-8')
@@ -82,7 +85,8 @@ def listen():
     def respond_to_client():
         while True:
             global current_time
-            current_time = datetime.datetime.now().strftime("%H:%M:%S")
+            current_time = datetime.now().strftime("%c")
+            # current_time = datetime.now().strftime("%H:%M:%S")
             _data = json.dumps({"clock": current_time})
             yield f"id: 1\ndata: {_data}\nevent: online\n\n"
             time.sleep(0.5)
